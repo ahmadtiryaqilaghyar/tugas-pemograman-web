b6ce1f640c18b1cdaeb26ae67781149bb9246b16
ahmadtiryaqilaghyar <ahmadtiryaqil@gmail.com>
2024-10-17 09:24:59 +0700

    Add files via upload

---
+++ scripttt.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML = "Belajar InnerHTML JS";
+document.write("Ini hasil dari Document.write JS");
+window.alert("Ini Alert dari JS");
+
+console.log("Ini hasil dari Console Log");
+console.info("Ini hasil Console Info");
+console.warn("Ini hasil dari Console Warning");
+console.error("Ini hasil Console Error");
+
+const dataTable = [
+    {"id": 1, "name": "paskal"},
+    {"id": 2, "name": "raffi"}
+]
+console.table(dataTable);
+console.info(dataTable);
